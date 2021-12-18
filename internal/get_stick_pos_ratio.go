bf3c46a4e5ff95470e95f446a35b3cfd323c2d5a
lapacek <simon.lapacek@gmail.com>
2021-12-19 00:30:08 +0100

    unit test initial commit

--- internal/get_stick_pos_ratio.go
+++ internal/get_stick_pos_ratio.go
@@ -2,18 +2,20 @@ package internal
 
 import (
 	"fmt"
-
 	"github.com/sirupsen/logrus"
+	"math"
+
+	//"github.com/sirupsen/logrus"
 )
 
 // stores the value of the approximated maximum/minimum stick position
-const limitPos float32 = 32000.0
+const limitPos float64 = 32000.0
 
 // provides the ratio of the stick position to the stick limit position, where
 // stick position is floating point number included in the [-32768.0, 32767.0]
-func GetStickPosRatio(input interface{}) (float32, error) {
+func GetStickPosRatio(input interface{}) (float64, error) {
 
-	stickPos, ok := input.(float32)
+	stickPos, ok := input.(float64)
 	if !ok {
 		msg := fmt.Sprintf("Invalid input(%v)", input)
 		logrus.Errorf(msg)
@@ -21,5 +23,7 @@ func GetStickPosRatio(input interface{}) (float32, error) {
 		return 0, fmt.Errorf(msg)
 	}
 
-	return stickPos / limitPos, nil
+	r := stickPos / limitPos
+
+	return math.Round(r*100)/100, nil
 }
