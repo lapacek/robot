74da0132f04aee2971f16dae67e87c56494c2c83
Simon Lapacek <slapacek@gwi.com>
2023-02-21 14:52:17 +0100

    code clean up

--- cmd/robot/robot.go
+++ cmd/robot/robot.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 
 	"robot/cmd/robot/cmd/tracker"
@@ -15,14 +13,6 @@ func NewRobotCommand() *cobra.Command {
 		Long: `The robot system control components.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
 
-			return nil
-		},
-		Args: func(cmd *cobra.Command, args []string) error {
-			for _, arg := range args {
-				if len(arg) > 0 {
-					return fmt.Errorf("%q does not take any arguments, got %q", cmd.CommandPath(), args)
-				}
-			}
 			return nil
 		},
 	}
